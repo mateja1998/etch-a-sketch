107ab1ac9e762acb7a13a8f7d993e712f6696a40
Mateja <293604994+mateja1998@users.noreply.github.com>
2026-07-05 14:56:33 +0200

    Generate and remove grid according to input

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
 const container = document.querySelector("#container");
+const gridButton = document.querySelector("#grid-btn");
 
-for(let i = 0 ; i < 256 ; i ++) {
-    let div = document.createElement("div");
-    div.setAttribute("id", `div${i+1}`);
-    div.classList.add("square");
-    container.appendChild(div);
+
+
+
+
+gridButton.addEventListener("click", () => {
+    removeOldGrid();
+    let gridSize = Number(prompt("Enter number of squares per one side of the grid:"));
+    if (gridSize > 100) {
+        console.log("You cannot choose number greater than 100. Please try again.");
+    } else {
+        generateGrid(gridSize);
+        const squares = document.querySelectorAll(".square");
+        squares.forEach(square => {
+            square.addEventListener("mouseenter", () => square.style.backgroundColor = "red");
+        })
+    }
+
+})
+
+function generateGrid(gridSize) {
+    for (let i = 0; i < gridSize ** 2; i++) {
+        let div = document.createElement("div");
+        div.setAttribute("id", `div${i + 1}`);
+        div.classList.add("square");
+        div.style.width = `calc((800px / ${gridSize}) - 2px)`
+        div.style.height = div.style.width;
+        container.appendChild(div);
+    }
 }
 
-const squares = document.querySelectorAll(".square");
-squares.forEach(square => {
-    square.addEventListener("mouseenter", () => square.style.backgroundColor = "red");
-})
+function removeOldGrid() {
+    if (document.querySelectorAll(".square")) {
+        const squares = document.querySelectorAll(".square");
+        squares.forEach(square => {
+            container.removeChild(square);
+            console.log("child removed");
+        })
+    }
+}
+
