698a68fb529855c42db064252733cc60c01e5d66
Mateja <293604994+mateja1998@users.noreply.github.com>
2026-07-05 11:14:03 +0200

    Fix 4x4 grid into 16x16

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector("#container");
 
-for(let i = 0 ; i < 16 ; i ++) {
+for(let i = 0 ; i < 256 ; i ++) {
     let div = document.createElement("div");
     div.setAttribute("id", `div${i+1}`);
     container.appendChild(div);
