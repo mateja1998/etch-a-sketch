90d4e345bf92db997ba5d2b2c780c88ede5026de
Mateja <293604994+mateja1998@users.noreply.github.com>
2026-07-05 14:21:23 +0200

    Create a button that changes grid size

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ const container = document.querySelector("#container");
 for(let i = 0 ; i < 256 ; i ++) {
     let div = document.createElement("div");
     div.setAttribute("id", `div${i+1}`);
+    div.classList.add("square");
     container.appendChild(div);
 }
+
+const squares = document.querySelectorAll(".square");
+squares.forEach(square => {
+    square.addEventListener("mouseenter", () => square.style.backgroundColor = "red");
+})
